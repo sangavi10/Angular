c70a2670d4f7075b09df5e0d5c1b4d7a66c312fd
sangavi10 <58692467+sangavi10@users.noreply.github.com>
2019-12-29 19:39:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,3 +11,28 @@ client.connect(url,{ useNewUrlParser: true }, function(err,db)
     }); 
     db.close(); 
 }); 
+
+
+node-demo
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('app.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(4200);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var claim='';
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("Demo");
+  dbo.collection("Example").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
